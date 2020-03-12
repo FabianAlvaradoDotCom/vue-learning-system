1660b82f39aee240a439725affc12f500b142af5
Luis Fabian Alvarado Ramirez <luisfabianalvaradoramirez@Itexicos-MacBook-Pro.local>
2020-03-12 12:04:26 -0600

    Capturing questions

--- src/pages/CreateQuestionPage.vue
+++ src/pages/CreateQuestionPage.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-        Create a new question
-    </div>
+  <div>
+    Create a new question
+    <form>
+      <label for="Question">
+        Question
+        <input type="text" v-model="question_text"/>
+      </label>
+      <br />
+      <button type="button" @click="submitQuestion">Submit new question</button>
+    </form>
+    <p>{{ question_text }}</p>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.prototype.$http = axios;
 export default {
-    
-}
+  methods: {
+    submitQuestion: function(e) {
+      e.preventDefault();
+      this.$http
+        .post('http://localhost:5000/create-question', {origin: "app", question: this.question_text})
+    }
+  },
+  data() {
+    return {
+      propi: 0,
+      question_text: ""
+    };
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
