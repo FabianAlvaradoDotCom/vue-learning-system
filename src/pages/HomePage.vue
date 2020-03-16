dc2feefc5d02979a08e8c63bee536fd14c34f00a
Luis Fabian Alvarado Ramirez <luisfabianalvaradoramirez@Itexicos-MacBook-Pro.local>
2020-03-16 05:51:38 -0600

    Home updated

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,34 @@
 <template>
-    <div>something</div> 
+    <div>
+        <h3>Question topics</h3>
+        <span v-for="subject in subjects" :key="subject">{{ subject }}</span>
+    </div> 
 </template>
 
 <script>
+
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.prototype.$http = axios;
+
 export default {
-    
+    data(){
+        return{
+            subjects:[]
+        }
+    },
+    methods:{
+
+    },
+    created(){
+        this.$http
+            .get('http://localhost:5000/get-questions')
+            .then( response => {
+                this.subjects = [...response.data.message.map((element) => element.subject).filter((element, index, array) => element !== array[index -1])];
+                console.log(this.subjects);
+            })
+    }
 }
 </script>
 <style scoped>
@@ -12,3 +36,5 @@ export default {
     color: red;
 }
 </style>
+
+hpiPa559u0rd
